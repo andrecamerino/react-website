b017dbe258a0ab17969c29798ad3dbd7e63a7c53
andrecamerino <camerinoandre@gmail.com>
2024-12-01 15:39:20 +1300

    add programs section

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subtitle="Our Programs" title="What We Offer"/>
+        <Programs />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
